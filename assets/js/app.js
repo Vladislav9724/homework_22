a0f0168febac1875b6d73d0c7b1b49678e57edec
Vladislav9724 <vlad.tsyapka@gmail.com>
2022-05-10 10:17:34 +0300

    v.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,13 +17,13 @@ function imt() {
         meningPlace.innerHTML = "виражений дефіци маси тіла";
     } else if((index > 16) && (index < 18.5)){
         meningPlace.innerHTML = 'недастатня маса тіла';
-    } else if((index > 18.5) && (index < 25)) {
+    } else if((index >= 18.5) && (index < 25)) {
         meningPlace.innerHTML = 'норма';
-    } else if((index => 25) && (index < 30)){
+    } else if((index >= 25) && (index < 30)){
         meningPlace.innerHTML = 'надмірна маса тіла';
-    } else if((index > 30) && (index < 35)){
+    } else if((index >= 30) && (index < 35)){
         meningPlace.innerHTML = 'ожиріння першої категорії';
-    } else if((index > 35) && (index < 40)){
+    } else if((index >= 35) && (index < 40)){
         meningPlace.innerHTML = 'ожиріння другої категорії';
     } else if(index >= 40){ 
         meningPlace.innerHTML = "ожиріння третьої котегорії"; 
